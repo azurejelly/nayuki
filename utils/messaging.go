e0e9f5fd862949ec5b3f3028e9e0e13b0bc9d571
azurejelly <178000437+azurejelly@users.noreply.github.com>
2025-06-23 19:41:02 -0400

    feat(utils): add messaging utils
    I'm tired of typing discordgo.InteractionResponseDeferredChannelMessageWithSource over and over again

---
+++ utils/messaging.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func Reply(s *discordgo.Session, i *discordgo.Interaction, str string) error {
+	return s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: str,
+		},
+	})
+}
+
+func ReplyEphemeral(s *discordgo.Session, i *discordgo.Interaction, str string) error {
+	return s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: str,
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	})
+}
+
+func Defer(s *discordgo.Session, i *discordgo.Interaction) error {
+	return s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+	})
+}
+
+func DeferEphemeral(s *discordgo.Session, i *discordgo.Interaction) error {
+	return s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags: discordgo.MessageFlagsEphemeral,
+		},
+	})
+}
+
+func UpdateDeferred(s *discordgo.Session, i *discordgo.Interaction, str string) error {
+	_, err := s.FollowupMessageCreate(i, false, &discordgo.WebhookParams{
+		Content: str,
+	})
+
+	return err
+}
+
+func UpdateDeferredEphemeral(s *discordgo.Session, i *discordgo.Interaction, str string) error {
+	_, err := s.FollowupMessageCreate(i, false, &discordgo.WebhookParams{
+		Content: str,
+		Flags:   discordgo.MessageFlagsEphemeral,
+	})
+
+	return err
+}

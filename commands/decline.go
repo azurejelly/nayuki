8b962d47588c1a5f13b44fd2e4515c6b58417834
azurejelly <178000437+azurejelly@users.noreply.github.com>
2025-07-03 10:09:22 -0400

    refactor(commands): attempt to reduce duplicate code

--- commands/decline.go
+++ commands/decline.go
@@ -1,14 +1,9 @@
 package commands
 
 import (
-	"fmt"
-	"log"
-	"time"
-
-	"github.com/azurejelly/nayuki/database"
+	"github.com/azurejelly/nayuki/helper"
 	"github.com/azurejelly/nayuki/utils"
 	"github.com/bwmarrin/discordgo"
-	embed "github.com/clinet/discordgo-embed"
 )
 
 type DeclineCommand struct{}
@@ -29,86 +24,8 @@ func (c *DeclineCommand) Command() *discordgo.ApplicationCommand {
 	}
 }
 
-// FIXME: duplicate code
 func (c *DeclineCommand) Run(s *discordgo.Session, event *discordgo.InteractionCreate) error {
 	i := event.Interaction
 	id := i.ApplicationCommandData().GetOption("id").StringValue()
-	utils.Defer(s, i)
-
-	suggestion, err := database.FindSuggestion(id)
-	if err != nil || suggestion == nil {
-		return utils.UpdateDeferred(s, i, ":x: Could not find a suggestion with that ID.")
-	}
-
-	server, err := database.GetOrCreateServer(i.GuildID)
-	if err != nil {
-		log.Println("something went wrong while communicating with the database:", err)
-		return utils.UpdateDeferred(s, i, fmt.Sprintf(":x: Failed to fetch and/or create server data: ```\n%s\n```", err.Error()))
-	}
-
-	err = database.DeleteSuggestion(suggestion)
-	if err != nil {
-		log.Println("something went wrong while communicating with the database:", err)
-		return utils.UpdateDeferred(s, i, fmt.Sprintf(":x: Could not delete suggestion from database: ```\n%s\n```", err.Error()))
-	}
-
-	result := ":white_check_mark: Suggestion declined."
-	msg, err := s.ChannelMessage(suggestion.Channel, suggestion.Message)
-	likes, dislikes := 0, 0
-
-	if err != nil {
-		result = fmt.Sprintf(":warning: The suggestion was declined, but the original message could not be fetched:\n```\n%s\n```", err.Error())
-	} else {
-		// Count likes and dislikes in the original message
-		likes = utils.CountReactions(msg, utils.LIKE_EMOJI, true)
-		dislikes = utils.CountReactions(msg, utils.DISLIKE_EMOJI, true)
-
-		// Lock the original discussion thread, if available
-		if msg.Thread != nil {
-			_, err := s.ChannelEditComplex(msg.Thread.ID, &discordgo.ChannelEdit{
-				Locked:   utils.Ptr(true),
-				Archived: utils.Ptr(true),
-			})
-
-			if err != nil {
-				result = fmt.Sprintf(":warning: The suggestion was declined, but its thread couldn't be locked/archived:\n```\n%s\n```", err.Error())
-			}
-		}
-	}
-
-	// Delete the original message
-	err = s.ChannelMessageDelete(suggestion.Channel, suggestion.Message)
-	if err != nil {
-		result = fmt.Sprintf(":warning: The suggestion was declined, but the original message couldn't be deleted: \n```\n%s\n```", err)
-		log.Println("could not delete original suggestion message:", err)
-	}
-
-	// TODO: make the embed a bit better
-	if server.LogsChannel != "" {
-		title := utils.Truncate(suggestion.Title, utils.MAX_TITLE_LENGTH)
-		content := utils.Truncate(suggestion.Content, utils.MAX_DESCRIPTION_LENGTH)
-
-		embed := embed.NewEmbed()
-		embed.SetTitle(title)
-		embed.SetDescription(content)
-		embed.AddField("Likes", fmt.Sprintf(":thumbs_up: %d", likes))
-		embed.AddField("Dislikes", fmt.Sprintf(":thumbs_down: %d", dislikes))
-		embed.AddField("Declined by", fmt.Sprintf("<@%s>", i.Member.User.ID))
-		embed.AddField("Suggested by", fmt.Sprintf("<@%s> (%s)", suggestion.Author, suggestion.AuthorName))
-		embed.SetFooter(fmt.Sprintf("ID: %s", suggestion.ID.Hex()))
-		embed.SetColor(utils.NEGATIVE_EMBED_COLOR)
-		embed.InlineAllFields()
-
-		u, _ := s.User(suggestion.Author)
-		if u != nil {
-			embed.SetAuthor("Suggestion declined.", u.AvatarURL("128"))
-		} else {
-			embed.SetAuthor("Suggestion declined.")
-		}
-
-		embed.Timestamp = time.Now().Format(time.RFC3339)
-		s.ChannelMessageSendEmbed(server.LogsChannel, embed.MessageEmbed)
-	}
-
-	return utils.UpdateDeferred(s, i, result)
+	return helper.TakeSuggestionAction(s, i, id, "declined", utils.POSITIVE_EMBED_COLOR)
 }
---
+++ helper/suggestion.go
@@ -0,0 +1,98 @@
+package helper
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/azurejelly/nayuki/database"
+	"github.com/azurejelly/nayuki/utils"
+	"github.com/bwmarrin/discordgo"
+	embed "github.com/clinet/discordgo-embed"
+)
+
+func TakeSuggestionAction(
+	s *discordgo.Session,
+	i *discordgo.Interaction,
+	id string,
+	verb string,
+	color int,
+) error {
+	utils.Defer(s, i)
+
+	suggestion, err := database.FindSuggestion(id)
+	if err != nil || suggestion == nil {
+		return utils.UpdateDeferred(s, i, ":x: Could not find a suggestion with that ID.")
+	}
+
+	server, err := database.GetOrCreateServer(i.GuildID)
+	if err != nil {
+		log.Println("something went wrong while communicating with the database:", err)
+		return utils.UpdateDeferred(s, i, fmt.Sprintf(":x: Failed to fetch and/or create server data: ```\n%s\n```", err))
+	}
+
+	err = database.DeleteSuggestion(suggestion)
+	if err != nil {
+		log.Println("something went wrong while communicating with the database:", err)
+		return utils.UpdateDeferred(s, i, fmt.Sprintf(":x: Could not delete suggestion from database: ```\n%s\n```", err))
+	}
+
+	result := fmt.Sprintf(":white_check_mark: Suggestion %s!", verb)
+	msg, err := s.ChannelMessage(suggestion.Channel, suggestion.Message)
+	likes, dislikes := 0, 0
+
+	if err != nil {
+		result = fmt.Sprintf(":warning: The suggestion was %s, but the original message could not be fetched:\n```\n%s\n```", verb, err)
+	} else {
+		// Count likes and dislikes in the original message
+		likes = utils.CountReactions(msg, utils.LIKE_EMOJI, true)
+		dislikes = utils.CountReactions(msg, utils.DISLIKE_EMOJI, true)
+
+		// Lock the original discussion thread, if available
+		if msg.Thread != nil {
+			_, err := s.ChannelEditComplex(msg.Thread.ID, &discordgo.ChannelEdit{
+				Locked:   utils.Ptr(true),
+				Archived: utils.Ptr(true),
+			})
+
+			if err != nil {
+				result = fmt.Sprintf(":warning: The suggestion was %s, but its thread couldn't be locked/archived:\n```\n%s\n```", verb, err)
+			}
+		}
+	}
+
+	// Delete the original message
+	err = s.ChannelMessageDelete(suggestion.Channel, suggestion.Message)
+	if err != nil {
+		result = fmt.Sprintf(":warning: The suggestion was %s, but the original message couldn't be deleted: \n```\n%s\n```", verb, err)
+		log.Println("could not delete original suggestion message:", err)
+	}
+
+	// TODO: make the embed a bit better
+	if server.LogsChannel != "" {
+		title := utils.Truncate(suggestion.Title, utils.MAX_TITLE_LENGTH)
+		content := utils.Truncate(suggestion.Content, utils.MAX_DESCRIPTION_LENGTH)
+
+		embed := embed.NewEmbed()
+		embed.SetAuthor(fmt.Sprintf("Suggestion %s!", verb))
+		embed.SetTitle(title)
+		embed.SetDescription(content)
+		embed.AddField("Likes", fmt.Sprintf(":thumbs_up: %d", likes))
+		embed.AddField("Dislikes", fmt.Sprintf(":thumbs_down: %d", dislikes))
+		embed.AddField("Reviewer", fmt.Sprintf("<@%s>", i.Member.User.ID))
+		embed.AddField("Suggested by", fmt.Sprintf("<@%s> (%s)", suggestion.Author, suggestion.AuthorName))
+		embed.SetFooter(fmt.Sprintf("ID: %s", suggestion.ID.Hex()))
+		embed.SetColor(color)
+		embed.InlineAllFields()
+
+		u, _ := s.User(suggestion.Author)
+		if u != nil {
+			embed.SetAuthor(embed.Author.Name, u.AvatarURL("1024"))
+		}
+
+		embed.Timestamp = time.Now().Format(time.RFC3339)
+		s.ChannelMessageSendEmbed(server.LogsChannel, embed.MessageEmbed)
+	}
+
+	return utils.UpdateDeferred(s, i, result)
+}
